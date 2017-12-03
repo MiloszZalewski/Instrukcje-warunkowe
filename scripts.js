73e18421a6960113721e7b1a24e60ac7e39fb9e1
Miłosz Zalewski <zalewski.milosz@gmail.com>
2017-12-03 15:12:33 +0100

    Add scripts file

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+var a = 1,
+	b = 2,
+	value = (a * a) - (2 * a * b) + (b * b);
+console.log ('The current value is: ', value);
+
+if (value >=0) {
+	console.log ('Wynik dodatni');
+
+} else {
+	console.log ('Wynik ujemny');
+}
