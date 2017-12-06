6057d28af56f795981ceda2d36efcb57e0473e29
Miłosz Zalewski <zalewski.milosz@gmail.com>
2017-12-06 20:56:33 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,12 +4,16 @@ var a = 1,
 	value = (a * a) - (2 * a * b) + (b * b);
 console.log ('The current value is: ', value);
 
-if (value > 0) {
+if (value >= 0) {
 	console.log ('Wynik dodatni');
-
-} else if (value = 0) {
-	console.log ('Wynik równy zero');
 
 } else {
 	console.log ('Wynik ujemny');
+}
+
+if (value = 0) {
+	console.log ('Wynik równy zero');
+
+} else {
+	console.log ('Wynik nie jest równy zero');
 }
