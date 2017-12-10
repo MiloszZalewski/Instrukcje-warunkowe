4ed25f0e9a5e0ae894b7280d0133f6ffabf65487
Miłosz Zalewski <zalewski.milosz@gmail.com>
2017-12-10 14:32:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 var a = 1,
 	b = 2,
 	value = (a * a) - (2 * a * b) + (b * b);
-console.log ('The current value is: ', value);
+	console.log ('The current value is: ', value);
 
 if (value > 0) {
 	console.log ('Wynik dodatni');
